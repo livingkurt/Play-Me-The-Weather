e37ba6361facf5c1826ac1f71c827775db19167e
keithbooher <keith.booher@yahoo.com>
2020-03-09 09:29:57 -0500

    committing progress

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css.scss';
 import Home from './components/pages/Home.jsx'
+import axios from 'axios'
 
 function App() {
+
+  const search = async () => {
+    const res = await axios.get('https://api.spotify.com/v1/search?q=roadhouse%20blues')
+    console.log(res.data)
+  }
+  useEffect(() => {
+    search()
+  }, [])
+
   return (
     <div className="main_container">
       <nav className="nav_container">
         <h3 className="app_title">Weather Music App</h3>
+        {/* <button>Sign In With Spotify</button> */}
       </nav>
       <Home />
     </div>
